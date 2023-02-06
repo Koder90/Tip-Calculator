fd5008bf733772aba0d21a2275377535a3cb7c5c
Koder90 <jerko.zlopasa@hotmail.com>
2023-02-06 13:20:01 +0000

    adding styling

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="bill">
+                <label>Bill</label>
+                    <input className='input' type="text" placeholder="$"/>
+                    <div className="btn-group">
+                <button className='button'>5%</button>
+                <button className='button'>10%</button>
+                <button className='button'>15%</button>
+                <button className='button'>25%</button>
+                <button className='button'>50%</button>
+                <button className='button'>CUSTOM</button>
+                </div>
+                <label>Number of people</label>
+                <input className='input' type="text" />
+            </div>
+            <div className="total">
+                <p>Tip Amount</p>
+                <p>Total</p>
+                <button type="button">RESET</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
